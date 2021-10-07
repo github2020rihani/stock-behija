8b7af903d4079819df6e5029f0fa7647b8578d3c
behija90 <54935739+behija90@users.noreply.github.com>
2021-10-07 10:36:33 +0100

    ajout "C:\php\pear"

--- public/index.php
+++ public/index.php
@@ -2,7 +2,9 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php;C:\php\pear';
+
+
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
